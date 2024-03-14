b2b254b32d090782674716d852252e4d30eb987e
palthisowmya-nichebit <sowmya.p@nichebit.com>
2024-03-14 10:49:20 +0530

    Initial Commit

---
+++ src/main/java/com/SpringApplication/SpringProjectDev1Application.java
@@ -0,0 +1,13 @@
+package com.SpringApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectDev1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectDev1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringApplication/SpringProjectDev1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectDev1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
